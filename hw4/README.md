f0631f8034eb8bc3db7512f03a84455b10243d3b
mfaruqui <manaalfar@gmail.com>
2016-04-04 23:23:33 -0400

    added the python file

--- hw4/README.md
+++ hw4/README.md
@@ -1,7 +1,7 @@
 There are two Python programs here:
 
  - `python bleu.py your-output.txt ref.txt` to compute the BLEU score of your output against the reference translation.
- - `python lm.py` trains an LSTM language model, just for your reference if you want to use pyCNN to perform this assignment.
+ - `python rnnlm.py ref.txt` trains an LSTM language model, just for your reference if you want to use pyCNN to perform this assignment.
 
 The `data/` directory contains the files needed to develop the MT system:
 
---
+++ hw4/rnnlm.py
@@ -0,0 +1,70 @@
+from pycnn import *
+import random
+
+LAYERS = 1
+INPUT_DIM = 50
+HIDDEN_DIM = 50
+VOCAB_SIZE = 0
+
+from collections import defaultdict
+from itertools import count
+import sys
+import util
+
+class RNNLanguageModel:
+    def __init__(self, model, LAYERS, INPUT_DIM, HIDDEN_DIM,
+                 VOCAB_SIZE, builder=SimpleRNNBuilder):
+        self.m = model
+        self.builder = builder(LAYERS, INPUT_DIM, HIDDEN_DIM, model)
+
+        model.add_lookup_parameters("lookup", (VOCAB_SIZE, INPUT_DIM))
+        model.add_parameters("R", (VOCAB_SIZE, HIDDEN_DIM))
+        model.add_parameters("bias", (VOCAB_SIZE))
+
+    def BuildLMGraph(self, sent):
+        renew_cg()
+
+        # Initialize the decoder with the avergae of input word vectors
+        vec = lookup(self.m["lookup"], int(sent[0]))
+        for word in sent[1:]:
+            vec += lookup(self.m["lookup"], int(word))
+        vec /= len(sent)
+        init = [tanh(vec), vec]
+        init_state = self.builder.initial_state(init)
+
+        R = parameter(self.m["R"])
+        bias = parameter(self.m["bias"])
+        errs = [] # will hold expressions
+        es=[]
+        state = init_state
+        for (cw,nw) in zip(sent,sent[1:]):
+            x_t = lookup(self.m["lookup"], int(cw))
+            state = state.add_input(x_t)
+            y_t = state.output()
+            r_t = bias + (R * y_t)
+            err = pickneglogsoftmax(r_t, int(nw))
+            errs.append(err)
+        nerr = esum(errs)
+        return nerr
+
+if __name__ == '__main__':
+    train = util.CorpusReader(sys.argv[1])
+    vocab = util.Vocab.from_corpus(train)
+    VOCAB_SIZE = vocab.size()
+
+    model = Model()
+    sgd = AdadeltaTrainer(model)
+    lm = RNNLanguageModel(model, LAYERS, INPUT_DIM, HIDDEN_DIM,
+                          VOCAB_SIZE, builder=LSTMBuilder)
+
+    train = list(train)
+    for ITER in xrange(100):
+        random.shuffle(train)
+        loss = 0.0
+        for i,sent in enumerate(train):
+            isent = [vocab.w2i[w] for w in sent]
+            errs = lm.BuildLMGraph(isent)
+            loss += errs.scalar_value()
+            errs.backward()
+            sgd.update(1.0)
+        print "ITER",ITER,loss
